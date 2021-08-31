bfde266c16b6693c7bc7cf214ce912a839b7f0eb
thekonly <joaopedro_m@icloud.com>
2021-08-30 22:06:37 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(character="*") {
+    return function(papaya="special") {
+        return `You are ${character}${papaya}${character}!`
+    }
+}
